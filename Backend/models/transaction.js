82526d9e0cdb6c86eff02b42f79edf0682015f31
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-29 18:21:12 +0100

    create transaction model

---
+++ Backend/models/transaction.js
@@ -0,0 +1,29 @@
+import { model, Schema, SchemaTypes } from "mongoose";
+
+const transactionSchema = {
+  type: {
+    type: String,
+    enum: ["Income, Expense"],
+    required: [true, "Transaction type is required"],
+  },
+  category: {
+    type: SchemaTypes.ObjectId,
+    ref: "category",
+    required: [true, "Transaction category is required"],
+  },
+  sum: {
+    type: Number,
+    required: [true, "Transaction sum is required"],
+  },
+  comment: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: [true, "Transaction date is required"],
+  },
+};
+
+const transaction = new Schema(transactionSchema);
+
+export default model("transaction", transaction);
