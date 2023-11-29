c5dfced7f6ab38244101fee5523bb3006c514fce
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-29 19:55:13 +0100

    implement setup routes script

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import { corsPlugin, bodyParserPlugin } from "./plugins/corsPlugin.js";
 import { loggerPlugin } from "./plugins/loggerPlugin.js";
+import { setupRoutes } from "./routes.js";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -10,5 +11,6 @@ const app = express();
 corsPlugin(app);
 bodyParserPlugin(app);
 loggerPlugin(app);
+setupRoutes(app);
 
 export default app;
