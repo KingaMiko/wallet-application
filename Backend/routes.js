7f6126ec33b11be7808dfcd6d3b150d7406b383d
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-29 23:15:07 +0100

    add express router

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,16 +1,22 @@
+import { Router } from "express";
+
 import * as controllers from "./controllers/index.js";
 
 export const setupRoutes = (app) => {
-  app.post("/auth/signup", controllers.authSignup);
-  app.post("/auth/signin", controllers.authSignin);
-  app.get("/auth/logout", controllers.authLogout);
+  const router = Router();
 
-  app.post("/transactions", controllers.addTransaction);
-  app.get("/transactions", controllers.getTransactions);
+  router.post("/auth/signup", controllers.authSignup);
+  router.post("/auth/signin", controllers.authSignin);
+  router.get("/auth/logout", controllers.authLogout);
 
-  app.get("/categories/:id", controllers.getCategory);
+  router.post("/transactions", controllers.addTransaction);
+  router.get("/transactions", controllers.getTransactions);
 
-  app.get("/statistics", controllers.getStatistics);
+  router.get("/categories/:id", controllers.getCategory);
 
-  app.get("/users/current", controllers.getUser);
+  router.get("/statistics", controllers.getStatistics);
+
+  router.get("/users/current", controllers.getUser);
+
+  app.use("/api", router);
 };
