bb8bd9cb212d7a6e03fc4d6a4df393ea80f51a67
GregorJosh <grzegorz.jozw@gmail.com>
2023-11-29 20:12:57 +0100

    export new controllers

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -3,3 +3,17 @@ export * from "./auth/authSingin.js";
 export * from "./auth/authLogout.js";
 
 export * from "./transactions/addTransaction.js";
+export * from "./transactions/getTransactions.js";
+
+export * from "./categories/getCategory.js";
+
+export * from "./statistics/getStatistics.js";
+
+export * from "./users/getUser.js";
+
+export * from "./configurations/addConfiguration.js";
+export * from "./configurations/getConfiguration.js";
+export * from "./configurations/setConfiguration.js";
+
+export * from "./curriencies/addCurrency.js";
+export * from "./curriencies/getCurrency.js";
