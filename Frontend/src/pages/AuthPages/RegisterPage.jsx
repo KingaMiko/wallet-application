26a8eaa4662cb990a7dc9ec66bc447ec87921edf
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-01 13:47:03 +0100

    add display styles for RegistrationPage

--- Frontend/src/pages/AuthPages/RegisterPage.jsx
+++ Frontend/src/pages/AuthPages/RegisterPage.jsx
@@ -1,9 +1,19 @@
 import { RegistrationForm } from 'components';
+import styles from './AuthPages.module.scss';
 
 const RegisterPage = () => {
   return (
     <>
-      <RegistrationForm />
+      <div className={styles['wrapper-page']}>
+        <div className={styles['wrapper-img']}>
+          <div>obrazek</div>
+          <h1>Finance App</h1>
+        </div>
+
+        <div className={styles['wrapper-form']}>
+          <RegistrationForm />
+        </div>
+      </div>
     </>
   );
 };
