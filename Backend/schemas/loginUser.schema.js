1d1098b2896d871ec917f3528f6fde2e03b07daf
Kinga Mikołajczyk <115529387+KingaMiko@users.noreply.github.com>
2023-12-01 20:18:09 +0100

    Update loginUser.schema.js
    pattern bez : i ; ale z resztą znaków :)

--- Backend/schemas/loginUser.schema.js
+++ Backend/schemas/loginUser.schema.js
@@ -9,7 +9,7 @@ import Joi from "joi";
 export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string()
-    .pattern(/^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[-+*_.]+)/)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{}'\"\\|,.<>\/?~])/)
     .min(6)
     .required(),
 });
