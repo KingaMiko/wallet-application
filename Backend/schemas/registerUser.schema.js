c009a584386ea8694c5d6951e54e870ddbf7b17a
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-01 13:37:04 +0100

    create registerUser req body schema

---
+++ Backend/schemas/registerUser.schema.js
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+/**
+ * @typedef {object} RegisterUserSchema
+ * @property {string} email.required
+ * @property {string} password.required
+ * @property {string} name
+ */
+
+export const registerUserSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .pattern(/^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[\-+*_]+)/)
+    .min(6)
+    .required(),
+  name: Joi.string()
+    .pattern(/^[a-zA-ZąćęłńóśżźĄĆĘŁŃÓŚŻŹ]+$/)
+    .min(3),
+});
