b00b74bc78388c417c79122726e18a8349f6bc2f
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-01 16:26:39 +0100

    a masz tam kropeczke :D

--- Backend/schemas/registerUser.schema.js
+++ Backend/schemas/registerUser.schema.js
@@ -10,7 +10,7 @@ import Joi from "joi";
 export const registerUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string()
-    .pattern(/^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[\-+*_]+)/)
+    .pattern(/^(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[\-+*_.]+)/)
     .min(6)
     .required(),
   name: Joi.string()
