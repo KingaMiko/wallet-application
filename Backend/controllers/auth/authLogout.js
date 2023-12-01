65a6c4eae390d100d4c056e13d91995854d8d47c
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-01 19:49:04 +0100

    create authLogout controller

--- Backend/controllers/auth/authLogout.js
+++ Backend/controllers/auth/authLogout.js
@@ -1,6 +1,24 @@
-export const authLogout = (req, res, next) => {
-  return res.json({
-    statusCode: 200,
-    description: "Auth Logout",
-  });
+import User from "#models/user.js";
+
+/**
+ * GET /api/logout
+ *
+ * @security BearerAuth
+ * @return 204 - Ok
+ * @return {ResponseSchema} 400 - Error: Bad Request
+ */
+
+export const authLogout = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(req.user.id, { token: null });
+
+    req.user = null;
+  } catch (error) {
+    res.status(400).json({
+      statusCode: 400,
+      description: error.message,
+    });
+  }
+
+  return res.status(204).end();
 };
