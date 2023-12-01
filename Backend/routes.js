213b49d2d5deb309dce17fb24ff090232fa554c3
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-01 13:34:33 +0100

    add temporary swagger doc specs

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,22 +1,56 @@
 import { Router } from "express";
 
+import { auth } from "./plugins/authPlugin.js";
 import * as controllers from "./controllers/index.js";
 
 export const setupRoutes = (app) => {
   const router = Router();
 
-  router.post("/auth/signup", controllers.authSignup);
-  router.post("/auth/signin", controllers.authSignin);
-  router.get("/auth/logout", controllers.authLogout);
+  router.post("/signup", controllers.authSignup);
+  /**
+   * POST /api/signin
+   */
+  router.post("/signin", controllers.authSignin);
+  /**
+   * GET /api/logout
+   * 
+   * @security BearerAuth
+   */
+  router.get("/logout", auth, controllers.authLogout);
 
-  router.post("/transactions", controllers.addTransaction);
-  router.get("/transactions", controllers.getTransactions);
+  /**
+   * POST /api/transactions
+   * 
+   * @security BearerAuth
+   */
+  router.post("/transactions", auth, controllers.addTransaction);
+  /**
+   * GET /api/transactions
+   * 
+   * @security BearerAuth
+   */
+  router.get("/transactions", auth, controllers.getTransactions);
 
-  router.get("/categories/:id", controllers.getCategory);
+  /**
+   * GET /api/categories/:id
+   * 
+   * @security BearerAuth
+   */
+  router.get("/categories/:id", auth, controllers.getCategory);
 
-  router.get("/statistics", controllers.getStatistics);
+  /**
+   * GET /api/statistics
+   * 
+   * @security BearerAuth
+   */
+  router.get("/statistics", auth, controllers.getStatistics);
 
-  router.get("/users/current", controllers.getUser);
+  /**
+   * GET /api/users/current
+   * 
+   * @security BearerAuth
+   */
+  router.get("/users/current", auth, controllers.getUser);
 
   app.use("/api", router);
 };
