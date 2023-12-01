6f520276ab7af35eb30aa7a5dd22577c0cc97c43
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-01 15:13:39 +0100

    fix path

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ const LoginPage = lazy(() => import('pages/AuthPages/LoginPage'));
 export const App = () => {
   return (
     <Routes>
-      <Route path="home" element={<LoginPage />} />
+      <Route path="login" element={<LoginPage />} />
       <Route path="register" element={<RegisterPage />} />
     </Routes>
   );
