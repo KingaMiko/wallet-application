3fd2b9badaeb2d1d7a24423829d5b06cff765e7c
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-12-02 15:31:15 +0100

    Nowy_Loader_Header

--- Frontend/src/components/Loader/Loader.jsx
+++ Frontend/src/components/Loader/Loader.jsx
@@ -1,30 +1,23 @@
-import React, { useContext } from 'react';
-import { PacmanLoader } from 'react-spinners';
-import { ThemeContext } from 'styled-components';
-import { Wrapper, WrapperBig } from './Loader.styled';
+import PropTypes from 'prop-types';
+import { Hourglass } from 'react-loader-spinner';
+import css from './Loader.module.css';
 
-function Loader() {
-  const themeContext = useContext(ThemeContext);
+const Loader = ({ isVisible = true }) => {
   return (
-    <Wrapper>
-      <PacmanLoader color={themeContext.colors.sctiveColor} />
-    </Wrapper>
+    <Hourglass
+      height="180"
+      width="180"
+      wrapperClass={css.loader}
+      visible={isVisible}
+      ariaLabel="hourglass-loading"
+      wrapperStyle={{}}
+      colors={['#000000', '#ed9f72']}
+    />
   );
-}
+};
 
-function LoaderWrapper({ color }) {
-  const themeContext = useContext(ThemeContext);
-  const loaderColor = color
-    ? color
-    : themeContext.colors.activeColor || '#24cca7';
-  return (
-    <WrapperBig>
-      <Wrapper>
-        <PacmanLoader color={loaderColor} />
-      </Wrapper>
-    </WrapperBig>
-  );
-}
+Loader.propTypes = {
+  isVisible: PropTypes.bool,
+};
 
 export default Loader;
-export { LoaderWrapper };
