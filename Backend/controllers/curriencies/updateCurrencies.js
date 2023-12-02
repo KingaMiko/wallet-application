d0244dcb3ac685954912581f3ea5aa1ebdc47824
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-02 19:06:38 +0100

    create updateCurrencies controller

--- Backend/controllers/curriencies/updateCurrencies.js
+++ Backend/controllers/curriencies/updateCurrencies.js
@@ -1,6 +1,31 @@
-export const updateCurrencies = (req, res, next) => {
-  res.json({
-    statusCode: 200,
-    description: "Update Curriencies",
-  });
+import axios from "axios";
+
+import Currency from "#models/currency.js";
+
+/**
+ * POST /api/currencies
+ * 
+ * @return {ResponseWithDataSchema} 200 - Success
+ * @return {ResponseSchema} 400 - Error
+ */
+
+export const updateCurrencies = async (req, res, next) => {
+  try {
+    const { data } = await axios.get(
+      "http://api.nbp.pl/api/exchangerates/tables/C/"
+    );
+
+    await Currency.deleteMany();
+
+    const actual = data[0].rates;
+    const result = await Currency.insertMany(actual);
+
+    res.status(200).json({
+      statusCode: 200,
+      description: "Update Curriencies",
+      data: { result },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
