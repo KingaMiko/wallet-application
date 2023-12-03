cc044e8daa8fc7a255b6fe9793fd2e6ad56d7e98
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-12-03 13:22:10 +0100

    added state and sorting in transactions

--- Frontend/src/components/Stats/Stats.jsx
+++ Frontend/src/components/Stats/Stats.jsx
@@ -1,4 +1,9 @@
 // import css from './Stats.module.scss';
+import { Doughnut } from 'react-chartjs-2';
+
+{
+  /* <Doughnut data={...} /> */
+}
 
 export const Stats = () => {
   return <div>Statistics</div>;
