6801f1ffb0d380e34844f3e52602561695b83152
tomaspalica <tomasss.palica@gmail.com>
2023-12-03 15:56:52 +0100

    finish getUser

--- Backend/controllers/users/getUser.js
+++ Backend/controllers/users/getUser.js
@@ -1,21 +1,27 @@
+import User from "#models/user.js";
+
 /**
  * GET /api/users/current
  *
  * @security BearerAuth
  */
 
-export const getUser = (req, res, next) => {
-  // zbieram id urzytkownika przez auth midleware
-  // const ourUser = user.find({id})
-  // if(ourUser){
-  //   res.json({
-  //     statusCode: 200,
-  //     data: ourUser,
-  //   });
-  // } else {console.error("No User Found")}
+export const getUser = async (req, res, next) => {
+  const id = req.user.id;
+  try {
+    const ourUser = await User.find({ _id: id });
 
-  res.json({
-    statusCode: 200,
-    description: "Get User",
-  });
+    if (ourUser) {
+      res.json({
+        statusCode: 200,
+        data: ourUser,
+      });
+    }
+    return res.json({
+      statusCode: 404,
+      message: "User not Found",
+    });
+  } catch (error) {
+    return res.json({ statusCode: 400, message: error.message });
+  }
 };
