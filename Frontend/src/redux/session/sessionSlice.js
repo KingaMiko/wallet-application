baa76d49f3b4ed6649e58b6c004f30f4797b7cd7
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-03 23:01:43 +0100

    fix name

--- Frontend/src/redux/session/sessionSlice.js
+++ Frontend/src/redux/session/sessionSlice.js
@@ -4,7 +4,7 @@ import { signUp, signIn } from './operations';
 const initialState = {
   user: { name: null, email: null },
   token: null,
-  isLoggedIn: false,
+  isAuth: false,
 };
 
 const sessionSlice = createSlice({
@@ -14,12 +14,11 @@ const sessionSlice = createSlice({
     builder
       .addCase(signUp.fulfilled, (state, action) => {
         state.user = action.payload.user;
-        state.isLoggedIn = true;
       })
       .addCase(signIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
-        state.isLoggedIn = true;
+        state.isAuth = true;
       });
   },
 });
