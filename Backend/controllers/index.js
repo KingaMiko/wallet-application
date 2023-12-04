1169e49251a2646721fd5c4d38d493b06d2ae569
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-04 20:24:03 +0100

    export authRefresh controller

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -1,6 +1,7 @@
 export * from "./auth/authSignup.js";
 export * from "./auth/authSingin.js";
 export * from "./auth/authLogout.js";
+export * from "./auth/authRefresh.js";
 
 export * from "./transactions/addTransaction.js";
 export * from "./transactions/getTransactions.js";
