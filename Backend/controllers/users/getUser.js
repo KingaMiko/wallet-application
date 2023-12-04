8382611994ccc6332ccd9da825d9e425a1fbe647
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-04 16:01:43 +0100

    added creating new access token

--- Backend/controllers/users/getUser.js
+++ Backend/controllers/users/getUser.js
@@ -1,29 +1,50 @@
+import JWT from "jsonwebtoken";
+import { configDotenv } from "dotenv";
+
 import User from "#models/user.js";
 
 /**
  * GET /api/users/current
  *
  * @security BearerAuth
+ * @return {ResponseWithTokenSchema} 200 - Success
+ * @return {ResponseSchema} 404 - User Not Found
+ * @return {ResponseSchema} 400 - Error
  */
 
 export const getUser = async (req, res, next) => {
-  const id = req.user.id;
   try {
-    const ourUser = await User.findOne({ _id: id });
+    const user = await User.findOne({ _id: req.user.id });
 
-    if (!ourUser) {
+    if (!user) {
       return res.status(404).json({
         statusCode: 404,
-        description: "User not Found",
+        description: "Current user not found.",
       });
     }
+
+    const { id, name, email, refreshToken } = user;
+
+    configDotenv();
+    const decodedRefreshToken = JWT.verify(
+      refreshToken,
+      process.env.REFRESH_SECRET_KEY
+    );
+
+    const newAccessToken = JWT.sign({ id, name }, process.env.SECRET_KEY, {
+      expiresIn: "10m",
+    });
+
     return res.status(200).json({
       statusCode: 200,
-      description: "Users data",
-      data: ourUser,
+      description: "Current user successfuly refreshed.",
+      token: newAccessToken,
+      data: {
+        email,
+        name,
+      },
     });
   } catch (error) {
-    console.error("Error fetching user:", error);
-    return res.status(400).json({ statusCode: 400, description: error.message });
+    next(error);
   }
 };
