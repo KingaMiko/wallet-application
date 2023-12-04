b7f5970d111507c7b6df521aeb70a0ce14ae44c5
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-04 20:07:56 +0100

    remove refresh user logic

--- Backend/controllers/users/getUser.js
+++ Backend/controllers/users/getUser.js
@@ -1,6 +1,3 @@
-import JWT from "jsonwebtoken";
-import { configDotenv } from "dotenv";
-
 import User from "#models/user.js";
 
 /**
@@ -9,32 +6,21 @@ import User from "#models/user.js";
  * @security BearerAuth
  * @return {ResponseWithDataSchema} 200 - Success
  * @return {ResponseSchema} 404 - User Not Found
- * @return {ResponseSchema} 401 - Unauthorized
  * @return {ResponseSchema} 400 - Error
  */
 
 export const getUser = async (req, res, next) => {
   try {
-    const user = await User.findOne({ _id: req.user.id });
+    const user = await User.findOne({ _id: req.user.id }).lean();
 
     if (!user) {
       return res.status(404).json({
         statusCode: 404,
-        description: "Current user not found.",
+        description: "Current user not found",
       });
     }
 
     const { name, email } = user;
-
-    configDotenv();
-    const decodedRefreshToken = JWT.verify(
-      refreshToken,
-      process.env.REFRESH_SECRET_KEY
-    );
-
-    const newAccessToken = JWT.sign({ id, name }, process.env.SECRET_KEY, {
-      expiresIn: "10m",
-    });
 
     return res.status(200).json({
       statusCode: 200,
