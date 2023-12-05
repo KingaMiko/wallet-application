3887cd43d7d11e07fce2b8737477f5bac00594fb
GregorJosh <grzegorz.jozw@gmail.com>
2023-12-05 20:41:39 +0100

    remove duplicated code

--- Backend/controllers/auth/authRefresh.js
+++ Backend/controllers/auth/authRefresh.js
@@ -1,6 +1,7 @@
 import JWT from "jsonwebtoken";
 import { configDotenv } from "dotenv";
 
+import { createTokens, sendRefreshToken } from "#plugins/authPlugin.js";
 import User from "#models/user.js";
 import Session from "#models/session.js";
 
@@ -14,16 +15,15 @@ import Session from "#models/session.js";
 
 export const authRefresh = async (req, res, next) => {
   if (req.cookies?.jwt) {
-    const refreshToken = req.cookies.jwt;
-
     try {
       configDotenv();
-      const secret = process.env.SECRET_KEY;
+
       const refSecret = process.env.REFRESH_SECRET_KEY;
+      const refreshToken = req.cookies.jwt;
 
       const decodedRefreshToken = JWT.verify(refreshToken, refSecret);
-
       const user = await User.findOne({ _id: decodedRefreshToken.id });
+
       if (!user) {
         return res.status(404).json({
           statusCode: 404,
@@ -32,24 +32,14 @@ export const authRefresh = async (req, res, next) => {
       }
 
       const { id, name } = user;
-
-      const newAccessToken = JWT.sign({ id, name }, secret, {
-        expiresIn: "10m",
-      });
-      const newRefreshToken = await JWT.sign({ id, name }, refSecret, {
-        expiresIn: "1d",
-      });
+      const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+        await createTokens({ id, name });
 
       user.token = newAccessToken;
       user.refreshToken = newRefreshToken;
       await user.save();
 
-      res.cookie("jwt", newRefreshToken, {
-        httpOnly: true,
-        sameSite: "None",
-        secure: true,
-        maxAge: 24 * 60 * 60 * 1000,
-      });
+      sendRefreshToken(res, newRefreshToken);
 
       return res.status(200).json({
         statusCode: 200,
