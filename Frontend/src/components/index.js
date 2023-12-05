6b6f42003111b5b1c1cb9208e1a21f99d2190ebe
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-05 23:10:48 +0100

    export components

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -3,3 +3,5 @@ export * from './Button/Button';
 export * from './LoginForm/LoginForm';
 export * from './Background/Background';
 export * from './Box/Box';
+export * from './Input/Input';
+export * from './Icon/Icon';
