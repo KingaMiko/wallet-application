393947586e0d5a0f212649d5f3d9a753d3ae146d
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-06 23:34:39 +0100

    commented out

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,8 +5,8 @@ import { useDispatch } from 'react-redux';
 
 import { getPatterns } from 'redux/global/operations';
 import { refreshUser } from 'redux/session/operations';
-import { useAuth } from './hooks/useAuth';
-import Loader from 'components/Loader/Loader';
+// import { useAuth } from './hooks/useAuth';
+// import Loader from 'components/Loader/Loader';
 
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 import { PrivateRoute } from 'components/PrivateRoute/PrivateRoute';
@@ -18,17 +18,17 @@ const StatisticsPage = lazy(() => import('pages/Statistics/Statistics.jsx'));
 
 export const App = () => {
   const dispatch = useDispatch();
-  const { isRefreshing } = useAuth();
+  // const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
     dispatch(getPatterns());
   }, [dispatch]);
 
-  return isRefreshing ? (
-    <Loader />
-  ) : (
-    // return (
+  // return isRefreshing ? (
+  //   <Loader />
+  // ) : (
+  return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
