2058002a613a23afada1b890d94ddf87d74e2130
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-12-07 12:17:02 +0100

    deleteUserCategory controller added

---
+++ Backend/controllers/categories/deleteCategory.js
@@ -0,0 +1,40 @@
+import category from "#models/category.js";
+import transaction from "#models/transaction.js";
+
+/**
+ * DELETE /api/auth/categories/{id}
+ *
+ * @security BearerAuth
+ * @param {string} id.path.required - ID of the category to delete
+ * @return {ResponseSchema} 200 - Success, category deleted
+ * @return {ResponseSchema} 400 - Error: Bad Request
+ * @return {ResponseSchema} 404 - Error: Not Found
+ */
+
+export const deleteUserCategory = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+
+  try {
+    const categoryToDelete = await category.findOne({ _id: id, owner: userId });
+
+    if (!categoryToDelete) {
+      return res.status(404).json({
+        statusCode: 404,
+        description: "Category not found or does not belong to the user",
+      });
+    }
+
+    await category.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      statusCode: 200,
+      description: "Category successfully deleted",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      statusCode: 400,
+      description: error.message,
+    });
+  }
+};
