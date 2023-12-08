2b092b3f1b2da37148c2555750a29eb6e7b5af3b
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-08 19:00:55 +0100

    import icons

--- Frontend/src/components/AnimationLogin/AnimationLogin.jsx
+++ Frontend/src/components/AnimationLogin/AnimationLogin.jsx
@@ -1,15 +1,31 @@
 import styles from './AnimationLogin.module.scss';
 
+import { ReactComponent as IconTel } from '../../images/animation/login/tel.svg';
+import { ReactComponent as IconMan } from '../../images/animation/login/man.svg';
+import { ReactComponent as IconCash } from '../../images/animation/login/cash.svg';
+import { ReactComponent as IconBasket } from '../../images/animation/login/basket.svg';
+import { ReactComponent as IconCard } from '../../images/animation/login/card.svg';
+
 export const AnimationLogin = () => {
   return (
     <div className={styles['icon-box']}>
-      <div className={styles['icon-tel']}></div>
+      <div className={styles['icon-tel']}>
+        <IconTel />
+      </div>
 
-      <div className={styles['icon-man']}></div>
+      <div className={styles['icon-man']}>
+        <IconMan />
+      </div>
 
-      <div className={styles['icon-cash']}></div>
-      <div className={styles['icon-basket']}></div>
-      <div className={styles['icon-card']}></div>
+      <div className={styles['icon-cash']}>
+        <IconCash />
+      </div>
+      <div className={styles['icon-basket']}>
+        <IconBasket />
+      </div>
+      <div className={styles['icon-card']}>
+        <IconCard />
+      </div>
     </div>
   );
 };
