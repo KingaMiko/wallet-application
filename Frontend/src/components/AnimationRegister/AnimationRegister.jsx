0b5522944f574120c18828008390fa1f685bc041
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-08 20:21:27 +0100

    update delay

--- Frontend/src/components/AnimationRegister/AnimationRegister.jsx
+++ Frontend/src/components/AnimationRegister/AnimationRegister.jsx
@@ -1,6 +1,6 @@
 import styles from './AnimationRegister.module.scss';
 
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { ReactComponent as IconTel } from '../../images/animation/register/tel.svg';
 import { ReactComponent as IconWoman } from '../../images/animation/register/woman.svg';
@@ -18,6 +18,7 @@ export const AnimationRegister = () => {
       transition: {
         duration: 0.3,
         ease: 'easeOut',
+        delay: 0.3,
         // type: 'spring',
         // stiffness: 100,
         // damping: 10,
@@ -52,7 +53,7 @@ export const AnimationRegister = () => {
           initial={{ opacity: 0, scale: 1.3 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.2, delay: 0.2 }}
+          transition={{ duration: 0.2, delay: 0.1 }}
         >
           <IconCash />
         </motion.div>
