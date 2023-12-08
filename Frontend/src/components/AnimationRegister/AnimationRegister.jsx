6bdc1cf937f56b6a97d997c3327299ec9de738a8
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-08 19:11:37 +0100

    create template boxes for icons

---
+++ Frontend/src/components/AnimationRegister/AnimationRegister.jsx
@@ -0,0 +1,13 @@
+import styles from './AnimationRegister.module.scss';
+
+export const AnimationRegister = () => {
+  return (
+    <div className={styles['icon-box']}>
+      <div className={styles['icon-tel']}></div>
+
+      <div className={styles['icon-woman']}></div>
+
+      <div className={styles['icon-cash']}></div>
+    </div>
+  );
+};
