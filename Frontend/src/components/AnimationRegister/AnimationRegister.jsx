50db9b640241a344a21ebf3c7d8aeade4bfc1b21
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-08 19:13:24 +0100

    icon-woman - animate presence

--- Frontend/src/components/AnimationRegister/AnimationRegister.jsx
+++ Frontend/src/components/AnimationRegister/AnimationRegister.jsx
@@ -1,13 +1,51 @@
 import styles from './AnimationRegister.module.scss';
 
+import { AnimatePresence, motion } from 'framer-motion';
+
+import { ReactComponent as IconTel } from '../../images/animation/register/tel.svg';
+import { ReactComponent as IconWoman } from '../../images/animation/register/woman.svg';
+import { ReactComponent as IconCash } from '../../images/animation/register/cash.svg';
+
 export const AnimationRegister = () => {
+  const variants = {
+    hidden: {
+      x: '100',
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+        ease: 'easeOut',
+      },
+    },
+  };
+
   return (
     <div className={styles['icon-box']}>
-      <div className={styles['icon-tel']}></div>
+      <div className={styles['icon-tel']}>
+        <IconTel />
+      </div>
 
-      <div className={styles['icon-woman']}></div>
+      <div className={styles['icon-woman']}>
+        <AnimatePresence>
+          <motion.div
+            className="icon-man"
+            layout
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            style={{ zIndex: 100 }}
+          >
+            <IconWoman />
+          </motion.div>
+        </AnimatePresence>
+      </div>
 
-      <div className={styles['icon-cash']}></div>
+      <div className={styles['icon-cash']}>
+        <IconCash />
+      </div>
     </div>
   );
 };
