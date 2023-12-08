23cb724fd5c2a2685b0b5ba27c2b887ed2348db7
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-08 19:09:45 +0100

    change animation for auth forms Box - more delicate

--- Frontend/src/components/Box/Box.jsx
+++ Frontend/src/components/Box/Box.jsx
@@ -1,39 +1,18 @@
 import PropTypes from 'prop-types';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import styles from './Box.module.scss';
 
-const variants = {
-  hidden: {
-    x: '100',
-    opacity: 0,
-  },
-  visible: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-      type: 'tween',
-      stiffness: 200,
-    },
-  },
-};
-
 export const Box = ({ children, className }) => {
   return (
-    <>
-      <AnimatePresence>
-        <motion.div
-          className="box"
-          layout
-          initial="hidden"
-          animate="visible"
-          variants={variants}
-        >
-          <div className={`${styles['box']} ${className}`}>{children}</div>
-        </motion.div>
-      </AnimatePresence>
-    </>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <div className={`${styles['box']} ${className}`}>{children}</div>
+    </motion.div>
   );
 };
 
