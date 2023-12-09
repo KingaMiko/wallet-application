80f9bc312f1bdb1107f75f9c46e554f36439840b
Amadimm <amadeuszrolecki@gmail.com>
2023-12-09 13:09:13 +0100

    Update Header.jsx

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -15,41 +15,41 @@ const Header = () => {
   const handleLogout = () => dispatch(logOut());
 
 
-  const [isSettingsModalOpen, setSettingsModalOpen] = useState(false);
-  const [newCategory, setNewCategory] = useState('');
-  const [selectedType, setSelectedType] = useState('income'); 
+  // const [isSettingsModalOpen, setSettingsModalOpen] = useState(false);
+  // const [newCategory, setNewCategory] = useState('');
+  // const [selectedType, setSelectedType] = useState('income'); 
 
-  const [categories, setCategories] = useState([]);
+  // const [categories, setCategories] = useState([]);
 
-  const openSettingsModal = () => {
-    setSettingsModalOpen(true);
-  };
+  // const openSettingsModal = () => {
+  //   setSettingsModalOpen(true);
+  // };
 
-  const closeSettingsModal = () => {
-    setSettingsModalOpen(false);
-    setNewCategory('');
-    setSelectedType('income');
-  };
+  // const closeSettingsModal = () => {
+  //   setSettingsModalOpen(false);
+  //   setNewCategory('');
+  //   setSelectedType('income');
+  // };
 
-  const handleAddCategory = () => {
-    if (newCategory.trim() !== '') {
-      setCategories(prevCategories => [
-        ...prevCategories,
-        { id: Date.now(), name: newCategory, type: selectedType }
-      ]);
+  // const handleAddCategory = () => {
+  //   if (newCategory.trim() !== '') {
+  //     setCategories(prevCategories => [
+  //       ...prevCategories,
+  //       { id: Date.now(), name: newCategory, type: selectedType }
+  //     ]);
 
-      setNewCategory('');
-      setSelectedType('income'); 
-    }
-  };
+  //     setNewCategory('');
+  //     setSelectedType('income'); 
+  //   }
+  // };
 
 return (
     <header className={css.headerContainer}>
       <div className={css.headerWrapper}>
         <Logo />
-        <button onClick={openSettingsModal}>Settings</button>
+        {/* <button onClick={openSettingsModal}>Settings</button> */}
 
-        {isSettingsModalOpen && (
+        {/* {isSettingsModalOpen && (
           <div className={css.modalOverlay}>
             <div className={css.modalContent}>
               <span className={css.closeModal} onClick={closeSettingsModal}>&times;</span>
@@ -108,7 +108,7 @@ return (
               </table>
             </div>
           </div>
-        )}
+        )} */}
 
         <div className={css.headerSide}>
           {isAuth && (
