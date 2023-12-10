8a2aecf46447d494eba58d6cb221cb81bb33b2dc
tomaspalica <tomasss.palica@gmail.com>
2023-12-10 12:41:32 +0100

    finish getStatistics

--- Backend/controllers/statistics/getStatistics.js
+++ Backend/controllers/statistics/getStatistics.js
@@ -3,6 +3,10 @@ import { findTransactions } from "#helpers/transactionHelper.js";
 import category from "#models/category.js";
 import { getExpense } from "#helpers/transactionHelper.js";
 import { getIncome } from "#helpers/transactionHelper.js";
+import User from "#models/user.js";
+import transaction from "#models/transaction.js";
+import { calculateStats } from "#helpers/transactionHelper.js";
+import { eachMonthTransactions } from "#helpers/transactionHelper.js";
 
 /**
  * A date
@@ -23,20 +27,27 @@ import { getIncome } from "#helpers/transactionHelper.js";
  */
 
 export const getStatistics = async (req, res, next) => {
-  const { month, year } = req.body;
+  const { month, year, type } = req.body;
   const ObjectId = mongoose.Types.ObjectId;
   const ownerId = new ObjectId(req.user.id);
   try {
-    const transactions = await findTransactions(ownerId, month, year);
-    const categories = await category.find({});
-    const stats = categories.map((category) => ({
-      category: category.name,
-      total: transactions
-        .filter((el) => el.category?.toString() === category.id)
-        .reduce((previouseValue, element) => {
-          return (previouseValue += element.sum);
-        }, 0),
-    }));
+    const expenseTransactions = await getExpense(ownerId, month, year);
+    const incomeTransactions = await getIncome(ownerId, month, year);
+    const owner = await User.findById(ownerId);
+    const ourTransactions = await findTransactions(ownerId, year);
+    const categories = await category.find({ _id: { $in: owner.categories } });
+
+    const expenseStats = calculateStats(
+      categories,
+      expenseTransactions,
+      "expense"
+    );
+
+    const incomeStats = calculateStats(
+      categories,
+      incomeTransactions,
+      "income"
+    );
     const expanses = (await getExpense(ownerId, month, year)).reduce(
       (previouseValue, element) => {
         return (previouseValue += element.sum);
@@ -50,13 +61,17 @@ export const getStatistics = async (req, res, next) => {
       0
     );
 
+    const eachMonthStats = await eachMonthTransactions(ourTransactions);
+
     return res.status(200).json({
       statusCode: 200,
       description: "Users statistics",
       data: {
-        stats,
+        expenseStats,
+        incomeStats,
         expanses,
         income,
+        eachMonthStats,
       },
     });
   } catch (error) {
--- Backend/helpers/transactionHelper.js
+++ Backend/helpers/transactionHelper.js
@@ -12,16 +12,22 @@ export const updateTransactionById = (id, fields) => {
   );
 };
 
-export const findTransactions = (ownerId, month, year) => {
+export const findTransactions = (ownerId, year, month, type) => {
   return transaction.aggregate([
     {
       $match: {
         owner: ownerId,
         $expr: {
-          $and: [
-            { $eq: [{ $month: "$date" }, month] },
-            { $eq: [{ $year: "$date" }, year] },
-          ],
+          $cond: {
+            if: month,
+            then: {
+              $and: [
+                { $eq: [{ $month: "$date" }, month] },
+                { $eq: [{ $year: "$date" }, year] },
+              ],
+            },
+            else: { $and: [{ $eq: [{ $year: "$date" }, year] }] },
+          },
         },
       },
     },
@@ -60,11 +66,41 @@ export const getIncome = (ownerId, month, year) => {
         owner: ownerId,
         type: "Income",
         $expr: {
-          $and: [
-            { $eq: [{ $month: "$date" }, month] },
-            { $eq: [{ $year: "$date" }, year] },
-          ],
+          $cond: {
+            if: month,
+            then: {
+              $and: [
+                { $eq: [{ $month: "$date" }, month] },
+                { $eq: [{ $year: "$date" }, year] },
+              ],
+            },
+            else: { $and: [{ $eq: [{ $year: "$date" }, year] }] },
+          },
         },
+      },
+    },
+    {
+      $lookup: {
+        from: "categories", // Upewnij się, że nazwa kolekcji kategorii jest poprawna
+        localField: "category",
+        foreignField: "_id",
+        as: "categoryData",
+      },
+    },
+    {
+      $unwind: {
+        path: "$categoryData",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $project: {
+        type: 1,
+        category: "$categoryData.name", // Zamienia 'category' na 'name' z 'categoryData'
+        sum: 1,
+        comment: 1,
+        date: 1,
+        owner: 1,
       },
     },
     { $sort: { date: -1 } },
@@ -77,13 +113,74 @@ export const getExpense = (ownerId, month, year) => {
         owner: ownerId,
         type: "Expense",
         $expr: {
-          $and: [
-            { $eq: [{ $month: "$date" }, month] },
-            { $eq: [{ $year: "$date" }, year] },
-          ],
+          $cond: {
+            if: month,
+            then: {
+              $and: [
+                { $eq: [{ $month: "$date" }, month] },
+                { $eq: [{ $year: "$date" }, year] },
+              ],
+            },
+            else: { $and: [{ $eq: [{ $year: "$date" }, year] }] },
+          },
         },
+      },
+    },
+    {
+      $lookup: {
+        from: "categories", // Upewnij się, że nazwa kolekcji kategorii jest poprawna
+        localField: "category",
+        foreignField: "_id",
+        as: "categoryData",
+      },
+    },
+    {
+      $unwind: {
+        path: "$categoryData",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $project: {
+        type: 1,
+        category: "$categoryData.name", // Zamienia 'category' na 'name' z 'categoryData'
+        sum: 1,
+        comment: 1,
+        date: 1,
+        owner: 1,
       },
     },
     { $sort: { date: -1 } },
   ]);
+};
+
+export const calculateStats = (categories, transaction, type) => {
+  return categories
+    .filter((el) => el.type === type)
+    .map((category) => ({
+      category: category.name,
+      total: transaction
+        .filter((el) => el.category === category.name)
+        .reduce((previouseValue, element) => {
+          return (previouseValue += element.sum);
+        }, 0),
+    }));
+};
+
+export const eachMonthTransactions = (ourTransactions) => {
+  const monthly = {
+    Income: {},
+    Expense: {},
+  };
+  Object.keys(monthly).forEach((transaction_type) => {
+    return ourTransactions
+      .filter((transaction) => transaction.type === transaction_type)
+      .forEach((raw_transaction) => {
+        const month = raw_transaction.date.getMonth() + 1;
+        const amountToAdd = raw_transaction.sum;
+        const currentAmount = monthly[transaction_type][month] || 0;
+        monthly[transaction_type][month] = currentAmount + amountToAdd;
+      });
+  });
+  return monthly;
 };
