163d52562c12ad479070d6928bb6a9df4e3dc0a8
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-10 22:56:08 +0100

    back to first version

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const walletInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
