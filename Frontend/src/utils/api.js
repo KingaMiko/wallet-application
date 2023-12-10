aa08f54544e370d7693dc55953442e6353f9caea
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-10 22:30:20 +0100

    update

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const walletInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
