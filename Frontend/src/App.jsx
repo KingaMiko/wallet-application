5baa6c7c300deebee8f4cfdb44d76b9804634aa7
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-12-11 17:43:04 +0100

    fixed responsive view for various devices

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,6 +17,7 @@ const RegisterPage = lazy(() => import('pages/AuthPages/RegisterPage'));
 const LoginPage = lazy(() => import('pages/AuthPages/LoginPage'));
 const HomePage = lazy(() => import('pages/Home/Home.jsx'));
 const StatisticsPage = lazy(() => import('pages/Statistics/Statistics.jsx'));
+const CurrencyPage = lazy(() => import('pages/Currency/Currency.jsx'));
 const RedirectPage = lazy(() => import('pages/RedirectPage/RedirectPage.jsx'));
 
 export const App = () => {
@@ -49,6 +50,14 @@ export const App = () => {
             element={
               <PrivateRoute navigateTo="/login">
                 <StatisticsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="currency"
+            element={
+              <PrivateRoute navigateTo="/login">
+                <CurrencyPage />
               </PrivateRoute>
             }
           />
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -7,26 +7,43 @@ import { TransactionsManager } from 'components/TransactionsManager/TransactionM
 import React from 'react';
 import { ButtonAddTransactions } from 'components/ButtonAddTransactions/ButtonAddTransactions';
 import { Helmet } from 'react-helmet';
+import NavigationMobile from 'components/Navigation/NavigationMobile';
+import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
   //const [isModalOpen, setIsModalOpen] = useState(false);
+  const isMobileView = useMediaQuery({ maxWidth: 767 });
 
   return (
     <div className={css.background}>
       <Helmet>
         <title>Home</title>
       </Helmet>
-      <section className={css.dashboard_container}>
-        <div className={css.container}>
-          <div className={css.sidebar}>
-            <Navigation />
-            <Balance />
-            <CurrencyTable />
-          </div>
-          <div className={css.main}>
-            {/* <p>Add your first transaction</p> */}
-            <TransactionsManager />
-          </div>
+      <section>
+        <div className={css.wrapper}>
+          {isMobileView ? (
+            <div className={css.mainMobile}>
+              <NavigationMobile />
+              <Balance />
+              <TransactionsManager />
+            </div>
+          ) : (
+            <div className={css.mainTabletAndDesktop}>
+              <div className={css.sidebar}>
+                <div>
+                  {' '}
+                  <Navigation />
+                  <Balance />
+                </div>
+                <div>
+                  <CurrencyTable />
+                </div>
+              </div>
+              <div className={css.main}>
+                <TransactionsManager />
+              </div>
+            </div>
+          )}{' '}
         </div>
         <ButtonAddTransactions />
       </section>
