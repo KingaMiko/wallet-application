a04ffd09a4dc843fb737d67e0a1b71cafe1e254b
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-12-11 13:52:24 +0100

    modal logOut

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -2,17 +2,33 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { selectIsAuth, selectUser } from 'redux/session/selectors';
-import { selectIsModalSettingsOpen } from 'redux/global/selectors';
+import {
+  selectIsModalSettingsOpen,
+  selectIsModalLogoutOpen,
+} from 'redux/global/selectors';
 import { logOut } from 'redux/session/operations';
-import { setIsModalSettingsOpen } from 'redux/global/globalSlice';
+import {
+  setIsModalSettingsOpen,
+  setIsModalLogoutOpen,
+} from 'redux/global/globalSlice';
 
 import { Logo } from 'components/Logo/Logo';
 import { OpenSettingsModal } from './Categories';
+import { ModalLogout } from 'components/ModalLogout/ModalLogout';
 import sprite from 'images/icons.svg';
 import css from './Header.module.scss';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const isModalLogoutOpen = useSelector(selectIsModalLogoutOpen);
+
+  const handleLogoutClick = () => {
+    dispatch(setIsModalLogoutOpen(true));
+  };
+
+  const handleCloseModalLogout = () => {
+    dispatch(setIsModalLogoutOpen(false));
+  };
 
   const isAuth = useSelector(selectIsAuth);
   const user = useSelector(selectUser);
@@ -39,7 +55,10 @@ const Header = () => {
                       <use href={`${sprite}#icon-pencil2`}></use>
                     </svg>
                   </button>
-                  <button className={css.headerLogout} onClick={handleLogout}>
+                  <button
+                    className={css.headerLogout}
+                    onClick={handleLogoutClick}
+                  >
                     <p className={css.headerSideText}>Exit</p>
                     <svg icon="logout" width="18px" height="18px">
                       <use href={`${sprite}#icon-exit`}></use>
@@ -54,6 +73,7 @@ const Header = () => {
           )}
         </div>
       </div>
+      {isModalLogoutOpen && <ModalLogout onClose={handleCloseModalLogout} />}
     </header>
   );
 };
