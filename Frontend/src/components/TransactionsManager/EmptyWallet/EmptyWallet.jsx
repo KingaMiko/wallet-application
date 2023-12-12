80df39467366bf236943e337bcd639f8e9a416e6
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-12-12 13:12:38 +0100

    empty wallet

---
+++ Frontend/src/components/TransactionsManager/EmptyWallet/EmptyWallet.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ButtonAddTransactions } from '../../ButtonAddTransactions/ButtonAddTransactions';
+import walletImage from '../../../images/wallet.png';
+import css from './EmptyWallet.module.scss';
+
+export const EmptyWallet = () => {
+  return (
+    <div className={css.emptywallet}>
+      <p>Your Wallet is empty. Enter your first transaction.</p>
+      <ButtonAddTransactions />
+      <img src={walletImage} alt="Empty Wallet" />
+    </div>
+  );
+};
