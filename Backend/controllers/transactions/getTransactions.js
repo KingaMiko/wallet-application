02697f3ddb2f4492c3ba7bd49953905fe2a55c57
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2024-01-03 14:19:35 +0100

    getTransaction on backend actualization

--- Backend/controllers/transactions/getTransactions.js
+++ Backend/controllers/transactions/getTransactions.js
@@ -5,17 +5,29 @@ import { findTransactions } from "#helpers/transactionHelper.js";
  * GET /api/transactions
  *
  * @security BearerAuth
+ * @param {number} year.query - Year to filter transactions
+ * @param {number} month.query - Month to filter transactions
+ * @param {number} limit.query - Limit of transactions to retrieve
  * @return {ResponseWithDataSchema} 200 - succes
  * @return {ResponseSchema} 400 - Error: Bad Request
  */
+
 export const getTransactions = async (req, res, next) => {
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentYear = currentDate.getFullYear();
+  const { year, month, limit } = req.query;
   const ObjectId = mongoose.Types.ObjectId;
   const ownerId = new ObjectId(req.user.id);
+
   try {
-    const result = await findTransactions(ownerId, currentYear, currentMonth);
+    const gottenYear = year || new Date().getFullYear();
+    const gottenMonth = month || null;
+    const gottenLimit = limit ? parseInt(limit, 10) : null;
+
+    const result = await findTransactions(
+      ownerId,
+      gottenYear,
+      gottenMonth,
+      gottenLimit
+    );
 
     return res.status(200).json({
       statusCode: 200,
