634b05eaafb107d17c8c56b424027a880cb89d5d
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2024-01-04 15:09:40 +0100

    change text in EmptyWallet component

--- Frontend/src/components/TransactionsManager/EmptyWallet/EmptyWallet.jsx
+++ Frontend/src/components/TransactionsManager/EmptyWallet/EmptyWallet.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-
-import walletImage from '../../../images/wallet.png';
 import css from './EmptyWallet.module.scss';
 
 export const EmptyWallet = () => {
   return (
     <div className={css.container}>
       <div className={css.emptywallet}>
-        <p>Your Wallet is empty. Enter your first transaction.</p>
-        <img src={walletImage} alt="Empty Wallet" />
+        <p>No transactions to display</p>
       </div>
     </div>
   );
