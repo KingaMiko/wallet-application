b36a9389b421cdc76ea03334fdbcb719e1900e56
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2024-01-05 10:38:58 +0100

    add page in Get transaction endpiont

--- Backend/controllers/transactions/getTransactions.js
+++ Backend/controllers/transactions/getTransactions.js
@@ -7,32 +7,42 @@ import { findTransactions } from "#helpers/transactionHelper.js";
  * @security BearerAuth
  * @param {number} year.query - Year to filter transactions
  * @param {number} month.query - Month to filter transactions
- * @param {number} limit.query - Limit of transactions to retrieve
- * @return {ResponseWithDataSchema} 200 - succes
+ * @param {number} limit.query.optional - Limit of transactions to retrieve per page (default: 10)
+ * @param {number} page.query.optional - Page number of transactions data (default: 1)
+ * @return {ResponseWithDataSchema} 200 - Success response with transactions data and pagination info
  * @return {ResponseSchema} 400 - Error: Bad Request
  */
 
 export const getTransactions = async (req, res, next) => {
-  const { year, month, limit } = req.query;
+  const { year, month, limit, page } = req.query;
   const ObjectId = mongoose.Types.ObjectId;
   const ownerId = new ObjectId(req.user.id);
 
   try {
     const gottenYear = year || new Date().getFullYear();
     const gottenMonth = month || null;
-    const gottenLimit = limit ? parseInt(limit, 10) : null;
+    const gottenLimit = limit ? parseInt(limit, 10) : 10;
+    const gottenPage = page ? parseInt(page, 10) : 1;
+
+    const skip = (gottenPage - 1) * gottenLimit;
 
     const result = await findTransactions(
       ownerId,
       gottenYear,
       gottenMonth,
-      gottenLimit
+      gottenLimit,
+      skip
     );
+
+    const totalCount = await Transaction.countDocuments({ owner: ownerId });
 
     return res.status(200).json({
       statusCode: 200,
       description: "Users Transactions",
       data: result,
+      page: gottenPage,
+      limit: gottenLimit,
+      totalPages: Math.ceil(totalCount / gottenLimit),
     });
   } catch (error) {
     return res.status(400).json({
