68966083d3251bd8fcfe23d371c288960d2d2db1
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2024-04-11 19:02:41 +0200

    create test acount and button to log in

--- Frontend/src/components/LoginForm/LoginForm.jsx
+++ Frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { Formik, Form } from 'formik';
+import React from 'react';
 import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -21,6 +22,10 @@ export const LoginForm = () => {
     password: '',
   };
 
+  const testCredentials = {
+    email: 'kinga.mikola@gmail.com',
+    password: 'TestTest1+',
+  };
   const validationSchema = Yup.object().shape({
     email: Yup.string().email('Invalid email').required('Email is required'),
     password: Yup.string()
@@ -53,47 +58,53 @@ export const LoginForm = () => {
   };
 
   return (
-    <>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {() => (
-          <Form className={styles['login-form']} autoComplete="off">
-            <Input
-              type="email"
-              name="email"
-              placeholder="E-mail"
-              iconID="icon-baseline-email"
-              title="Enter your email"
-              onFocus={resetError}
-            />
-
-            <Input
-              type="password"
-              name="password"
-              placeholder="Password"
-              autoComplete="off"
-              iconID="icon-baseline-lock"
-              title="Enter your password"
-              onFocus={resetError}
-            />
-            {error && <div className={styles['error-message']}>{error}</div>}
-            <Button type="submit" theme="color">
-              Log in
-            </Button>
-
-            <Button
-              type="button"
-              theme="white"
-              onClick={() => navigate('/register')}
-            >
-              Register
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {formikProps => (
+        <Form className={styles['login-form']} autoComplete="off">
+          <Input
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            iconID="email-icon"
+            autoComplete="email"
+            onFocus={resetError}
+          />
+          <Input
+            type="password"
+            name="password"
+            iconID="password-icon"
+            placeholder="Password"
+            autoComplete="current-password"
+            onFocus={resetError}
+          />
+          {error && <div className={styles['error-message']}>{error}</div>}
+          <Button type="submit" theme="color">
+            Log in
+          </Button>
+          <Button
+            type="button"
+            theme="white"
+            onClick={() => navigate('/register')}
+          >
+            Register
+          </Button>
+          <Button
+            type="button"
+            theme="color"
+            onClick={() => {
+              formikProps.setFieldValue('email', testCredentials.email);
+              formikProps.setFieldValue('password', testCredentials.password);
+              setTimeout(formikProps.submitForm, 0);
+            }}
+          >
+            USE TEST ACOUNT
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
