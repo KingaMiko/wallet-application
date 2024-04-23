db6d60f1ca2215577a60c267f1700b42ec35da0c
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2024-04-23 16:32:56 +0200

    back to the working version of createCategory

--- Backend/controllers/categories/createCategory.js
+++ Backend/controllers/categories/createCategory.js
@@ -1,6 +1,7 @@
 import Category from "#models/category.js";
 import User from "#models/user.js";
 import Filter from "bad-words";
+const filter = new Filter();
 
 /**
  * @typedef {object} CategoryCreate
@@ -16,235 +17,18 @@ import Filter from "bad-words";
  * @return {ResponseWithDataSchema<CategoryCreate>} 201 - Success, category created
  * @return {ResponseSchema} 400 - Error: Bad Request
  */
-
-const customList = [
-  // Odmiany słowa "kurwa"
-  "kurwa",
-  "kurwie",
-  "kurwą",
-  "kurwę",
-  "kurwe",
-  "kurwo",
-  "kurwy",
-  "kurwach",
-  "kurwami",
-  "kurwiska",
-  "kurwiszcze",
-  "kurwiszon",
-  "kurwiszony",
-  "kurwiszonów",
-  "kurwojady",
-  "kurwojadów",
-
-  // Odmiany słowa "chuj"
-  "chuj",
-  "chuja",
-  "chujem",
-  "chuju",
-  "chujowi",
-  "chujów",
-  "chuje",
-  "chujek",
-  "chujami",
-  "chujoza",
-  "chujnia",
-  "chujowy",
-  "chujowa",
-  "chujowe",
-
-  // Odmiany słowa "pierdolić"
-  "pierdolę",
-  "pierdolić",
-  "pierdolisz",
-  "pierdoli",
-  "pierdolą",
-  "pierdolony",
-  "pierdolona",
-  "pierdolone",
-  "pierdolęcie",
-  "pierdolenie",
-  "pierdolnięty",
-  "pierdolnięta",
-  "pierdolnięte",
-  "spierdolić",
-  "spierdolił",
-  "opierdolić",
-  "opierdol",
-  "opierdoli",
-  "opierdala",
-
-  // Odmiany słowa "jebać"
-  "jebać",
-  "jeb",
-  "jebie",
-  "jebię",
-  "jebiesz",
-  "jebią",
-  "jebany",
-  "jebana",
-  "jebane",
-  "jebanka",
-  "jebanko",
-  "wyjebać",
-  "wyjebie",
-  "wyjebią",
-  "wyjebiesz",
-  "wyjebali",
-  "zajebisty",
-  "zajebista",
-  "zajebiste",
-  "zajebać",
-  "zajebie",
-  "zajebią",
-  "zajebiesz",
-  "zajebali",
-  "zjebać",
-  "zjebie",
-  "zjebią",
-  "zjebiesz",
-  "zjebane",
-  "dojebać",
-  "dojebie",
-  "dojebią",
-  "dojebiesz",
-  "dojebane",
-
-  // Odmiany słowa "spierdalać"
-  "spierdalaj",
-  "spierdalać",
-  "spierdalam",
-  "spierdalają",
-  "spierdalał",
-  "spierdalała",
-  "spierdalajcie",
-  "spierdolić",
-  "spierdoli",
-  "spierdolą",
-  "spierdolił",
-  "spierdoliła",
-  "spierdoliło",
-  "spierdolisz",
-  "spierdolicie",
-  "pospierdalać",
-  "pospierdalaj",
-  "pospierdala",
-  "pospierdali",
-
-  // Odmiany słowa "pizda"
-  "pizda",
-  "pizdzie",
-  "pizdą",
-  "pizdu",
-  "pizdy",
-  "pizdom",
-  "pizdach",
-  "pizdami",
-
-  // Inne wulgaryzmy i ich odmiany
-  "cipa",
-  "cipie",
-  "cipą",
-  "cipę",
-  "cipy",
-  "cipach",
-  "cipami",
-  "dupa",
-  "dupie",
-  "dupą",
-  "dupę",
-  "dupy",
-  "dupach",
-  "dupami",
-  "huj",
-  "huja",
-  "hujowi",
-  "hujem",
-  "huju",
-  "hujów",
-  "huje",
-  "hujami",
-  "dziwka",
-  "dziwki",
-  "dziwce",
-  "dziwkę",
-  "dziwko",
-  "dziwek",
-  "dziwką",
-  "dziwkami",
-  "dziwkach",
-  "cipa",
-  "cipie",
-  "cipą",
-  "cipę",
-  "cipy",
-  "cipach",
-  "cipami",
-  "dupa",
-  "dupie",
-  "dupą",
-  "dupę",
-  "dupy",
-  "dupach",
-  "dupami",
-  "huj",
-  "huja",
-  "hujowi",
-  "hujem",
-  "huju",
-  "hujów",
-  "huje",
-  "hujami",
-
-  // Błędne wersje literowe popularnych wulgaryzmów
-  "kvrwa",
-  "chvuj",
-  "pierdvlę",
-  "jebvny",
-  "spierdvlaj",
-  "pizdva",
-];
-
-const customFilter = new Filter({ emptyList: true });
-customFilter.addWords(...customList);
-
-const defaultFilter = new Filter();
-
-function createProfanityRegex(words) {
-  const pattern = words
-    .map((word) =>
-      word
-        .replace(/a/g, "[aą]")
-        .replace(/c/g, "[cć]")
-        .replace(/e/g, "[eę]")
-        .replace(/l/g, "[lł]")
-        .replace(/n/g, "[nń]")
-        .replace(/o/g, "[oó]")
-        .replace(/s/g, "[sś]")
-        .replace(/z/g, "[zźż]")
-        .split("")
-        .join("\\W*")
-    )
-    .join("|");
-
-  return new RegExp(`(${pattern})`, "gi");
-}
-
-const profanityRegex = createProfanityRegex(customList);
-
 export const createCategory = async (req, res) => {
   try {
-    const hasEnglishProfanity = defaultFilter.isProfane(value);
-    const hasCustomProfanity =
-      customFilter.isProfane(value) || profanityRegex.test(value);
     const { name, type } = req.body;
 
-    if (hasEnglishProfanity || hasCustomProfanity) {
-      return res
-        .status(400)
-        .json({ description: "Please avoid using offensive language" });
-    }
     if (name.length > 20) {
       return res.status(400).json({ description: "Name is too long" });
+    }
+
+    if (filter.isProfane(name)) {
+      return res
+        .status(400)
+        .json({ description: "Name contains prohibited words" });
     }
 
     const userId = req.user.id;
